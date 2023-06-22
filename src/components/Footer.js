af848ce10b27cb3c275b267a168553d307d06d65
kade <zaccaro.kade@gmail.com>
2023-06-21 21:54:53 -0400

    Embed Google Maps via iframe for location display

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,18 +3,29 @@ export const Footer = () => {
   footerContainer.classList.add("footer-container");
 
   footerContainer.innerHTML = `
-    <div>
-			<h2>Hours</h2>
-			<p>Monday to Thursday: 9 AM - 10 PM</p>
-			<p>Friday: 9 AM - 11 PM</p>
-			<p>Saturday: 10 AM - 11 PM</p>
-			<p>Sunday: 10 AM - 9 PM</p>
+    <div class="bg">
+      <div>
+        <h2>Hours</h2>
+        <p>Monday to Thursday: 9 AM - 10 PM</p>
+        <p>Friday: 9 AM - 11 PM</p>
+        <p>Saturday: 10 AM - 11 PM</p>
+        <p>Sunday: 10 AM - 9 PM</p>
+      </div>
+    
+      <div>
+        <h2>Location</h2>
+        <p>Hakone, Kanagawa, Japan</p>
+        <iframe
+          width="520"
+          height="320"
+          style="border: 0"
+          loading="lazy"
+          allowfullscreen
+          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJe9yyYAyiGWARJnuXrBe9BSY&key=AIzaSyBTgWy5qLs7kNUH62V8unr-tTdLIx5YH34"
+        >
+        </iframe>
+      </div>
     </div>
-
-    <div>
-			<h2>Location</h2>
-			<p>444 Pearl Street, Monterey, California</p>
-		</div>
   `;
 
   document.body.appendChild(footerContainer);
