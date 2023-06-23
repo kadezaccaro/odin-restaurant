f482683718583b24b7203b699e67c24a4473d6db
kade <zaccaro.kade@gmail.com>
2023-06-23 16:49:38 -0400

    Use map without API key and uninstall dotenv

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -16,12 +16,13 @@ export const Footer = () => {
         <h2>Location</h2>
         <p>Hakone, Kanagawa, Japan</p>
         <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3260.072483208705!2d139.02279247665976!3d35.20466345600456!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6019988bcd2ac2b5%3A0xaae4a36847c7e09a!2sHakone%20Shrine!5e0!3m2!1sen!2sus!4v1687553023153!5m2!1sen!2sus"
           width="520"
-          height="320"
+          height="360"
           style="border: 0"
+          allowfullscreen=""
           loading="lazy"
-          allowfullscreen
-          src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJe9yyYAyiGWARJnuXrBe9BSY&key=${process.env.API_KEY}"
+          referrerpolicy="no-referrer-when-downgrade"
         >
         </iframe>
       </div>
